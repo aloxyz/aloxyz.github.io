d8a93f67ea9044224d518aac3db3d5c7d548ef79
aloxyz <aloisiogabriele@gmail.com>
2023-01-27 23:57:23 +0100

    viteconfigjs

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   base: "/aloxyz.github.io/",
+  publicPath: 'https://aloxyz.github.io/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
