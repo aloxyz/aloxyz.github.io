7adf1d07e54c3e7f65a58a2d1b9f699bb0b60b75
aloxyz <aloisiogabriele@gmail.com>
2025-03-31 00:34:48 +0200

    edited base path

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/aloxyz.github.io/",
+  base: "/",
   publicPath: 'https://aloxyz.github.io/',
   resolve: {
     alias: {
